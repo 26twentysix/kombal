7556984c729bb8b2ced61d7a865445e9acb3fb52
lilangel <cvndyperov@gmail.com>
2022-03-27 16:43:55 +0500

    lab1 done

--- lab1.py
+++ lab1.py
@@ -96,6 +96,26 @@ def check_if_move_is_possible(current_node, move):
             return False
         elif move == dirs[6]:
             return False
+    elif current_node % 8 == 1:
+        if move == dirs[6]:
+            return False
+        elif move == dirs[7]:
+            return False
+    elif current_node % 8 == 6:
+        if move == dirs[2]:
+            return False
+        elif move == dirs[3]:
+            return False
+    elif int(current_node / 8) == 1:
+        if move == dirs[0]:
+            return False
+        elif move == dirs[1]:
+            return False
+    elif int(current_node / 8) == 6:
+        if move == dirs[4]:
+            return False
+        elif move == dirs[5]:
+            return False
     return True
 
 
@@ -107,21 +127,54 @@ def bfs(graph, start_node, end_node):
     while my_queue:
         current_node = my_queue.popleft()
         visited.add(current_node)
-        print(convert_index_to_node(current_node))
         if current_node == end_node:
-            print('Im done!')
             break
         for i in range(8):
             if check_if_move_is_possible(current_node, dirs[i]):
                 if current_node + dirs[i] not in visited and -1 < current_node + dirs[i] < len(graph)\
                         and current_node + dirs[i] not in bad_pos:
                     my_queue.append(current_node+dirs[i])
+    return visited
 
 
+def find_route(visited: set, start_node, end_node):
+    route = list()
+    route.append(end_node)
+    current_node = end_node
+    while start_node not in route:
+        visited.discard(current_node)
+        for i in range(8):
+            next_node = current_node + dirs[7 - i]
+            if next_node in visited:
+                route.append(next_node)
+                current_node = next_node
+                break
+            if i == 7:
+                route.pop()
+                visited.update(route)
+                route.clear()
+                route.append(end_node)
+                current_node = end_node
+    return route
+
+
+def write_route_to_file(filename, route):
+    with open(filename, 'w') as f:
+        j = '1'
+        for i in range(len(route) - 1):
+            turn_type = '—'
+            if i == len(route) - 2:
+                turn_type = 'x'
+            line = j + '. К' + convert_index_to_node(route[len(route) - i - 1]) + turn_type + convert_index_to_node(route[len(route) - i - 2]) + '\n'
+            f.write(line)
+            j = str(int(j) + 1)
+
+
+with open('testLab1.txt') as f:
+    lines = f.readlines()
 graph = '#'*64
-root_node = convert_node_to_index('b2')
-finish_node = convert_node_to_index('e6')
+root_node = convert_node_to_index(lines[0])
+finish_node = convert_node_to_index(lines[1])
 graph = graph[:root_node] + 'К' + graph[root_node + 1:]
 graph = graph[:finish_node] + 'П' + graph[finish_node + 1:]
-print(graph)
-bfs(graph, root_node, finish_node)
+write_route_to_file('testLab1Result.txt', find_route(bfs(graph, root_node, finish_node), root_node, finish_node))
